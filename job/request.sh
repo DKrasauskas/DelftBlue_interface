d520f4284ee20ab5f86cb93bb74183d85426a8bc
dominykas <d.krasauskas@student.tudelft.nl>
2025-10-30 20:10:57 +0100

    inital

--- job/request.sh
+++ job/request.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --job-name="job_DK"
 #SBATCH --partition=compute
-#SBATCH --time=00:00:10
+#SBATCH --time=00:30:10
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
 #SBATCH --mem-per-cpu=1M
